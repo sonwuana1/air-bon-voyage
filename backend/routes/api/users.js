1f307263f4410ac555dfaff9f82855bec3d7c03f
Sylvia Onwuana <sonwuana1@yahoo.com>
2021-06-29 14:29:15 -0700

    signup route created and tested

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,25 @@
 const express = require('express')
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+
 const router = express.Router();
 
+
+// Sign up
+router.post('/', asyncHandler(async (req, res) => {
+      const { email, password, first_name, last_name } = req.body;
+      const user = await User.signup({ email, first_name, last_name, password });
+
+      await setTokenCookie(res, user);
+
+      return res.json({
+        user,
+      });
+    }),
+);
 
 
 
