310a6cdd3b6463c6c717d77e0252e5f44b849c46
Sylvia Onwuana <sonwuana1@yahoo.com>
2021-06-29 14:50:13 -0700

    signup validation created and tested

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -3,13 +3,43 @@ const asyncHandler = require('express-async-handler');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 
 const router = express.Router();
 
 
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('first_name')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a First Name with at least 4 characters.'),
+  check('first_name')
+    .not()
+    .isEmail()
+    .withMessage('First Name cannot be an email.'),
+  check('last_name')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a Last Name with at least 4 characters.'),
+  check('last_name')
+    .not()
+    .isEmail()
+    .withMessage('Last Name cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors,
+];
+
 // Sign up
-router.post('/', asyncHandler(async (req, res) => {
+router.post('/', validateSignup, asyncHandler(async (req, res) => {
       const { email, password, first_name, last_name } = req.body;
       const user = await User.signup({ email, first_name, last_name, password });
 
