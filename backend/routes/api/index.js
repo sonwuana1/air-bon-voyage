4c2eb2cd66848a66cd3d893d24d7c36e6ca8d9be
Sylvia Onwuana <sonwuana1@yahoo.com>
2021-06-29 15:54:52 -0700

    frontend setup complete and removed backend test route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -7,10 +7,6 @@ const usersRouter = require('./users.js');
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 
-// testing setup
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-});
 
 
 
