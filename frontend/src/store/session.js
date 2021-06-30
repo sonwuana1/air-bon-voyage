abb52b0fab7d367c62983efd417917be8a404ff9
Sylvia Onwuana <sonwuana1@yahoo.com>
2021-06-29 17:31:53 -0700

    navigation component and profile button created

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -57,6 +57,14 @@ export const signup = (user) => async (dispatch) => {
   return response;
 };
 
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
+
 
 const initialState = { user: null };
 
