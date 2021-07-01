18ca439aab22d19fdf296f102c4c70ae1f128a31
Sylvia Onwuana <sonwuana1@yahoo.com>
2021-06-30 17:29:05 -0700

    backend routes for spots created and tested

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const spotsRouter = require('./spots.js')
 
 
 
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
+router.use('/spots', spotsRouter);
 
 
 
