ce509d6304f9f37ce05e9b5065d84f3a9a7a2843
Sylvia Onwuana <sonwuana1@yahoo.com>
2021-07-04 08:35:09 -0700

    created new thunk action to get one review

--- frontend/src/store/review.js
+++ frontend/src/store/review.js
@@ -2,11 +2,17 @@ import { csrfFetch } from './csrf';
 
 
 const LOAD_REVIEW = 'review/LOAD_REVIEW';
+const ADD_ONE_REVIEW = 'review/ADD_ONE_REVIEW';
 
 
 const load = reviews => ({
     type: LOAD_REVIEW,
     reviews,
+});
+
+const addOneReview = review => ({
+    type: ADD_ONE_REVIEW,
+    review,
 });
 
 
@@ -22,6 +28,18 @@ export const getAllReviews = () => async (dispatch) => {
 };
 
 
+export const getOneReview = (id) => async (dispatch) => {
+    const response = await csrfFetch(`/api/reviews/${id}`);
+
+    if (response.ok) {
+      const data = await response.json();
+    //   console.log('DATAAAAAAAAA', data)
+      dispatch(addOneReview(data));
+    }
+    return response;
+}
+
+
 
 const initialState = {}
 
@@ -34,6 +52,14 @@ const reviewReducer = (state=initialState, action) => {
                 newState[review.id] = review;
             })
             return newState;
+        case ADD_ONE_REVIEW:
+            if (!state[action.review.id]) {
+                const newState = { ...state, ...action.review }
+                return newState;
+            }
+            return {
+                ...state, ...state[action.review.id], ...action.review
+            }
         default:
             return state;
     }
