d5b1da1bf03637be80dd475b5ce899d44c2719b4
Sylvia Onwuana <sonwuana1@yahoo.com>
2021-07-07 21:06:02 -0700

    added a modal for delete reviews

--- frontend/src/components/DeleteReview/index.js
+++ frontend/src/components/DeleteReview/index.js
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
 import { deleteReview } from '../../store/review';
 import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
 
 
 function DeleteReview({ props }) {
@@ -9,6 +11,11 @@ function DeleteReview({ props }) {
     const dispatch = useDispatch();
     const { id } = useParams();
     // console.log(id)
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const reviewState = useSelector(state => state.review)
     // console.log('REVIEWSTATE', reviewState)
@@ -24,7 +31,26 @@ function DeleteReview({ props }) {
 
     return (
         <div>
-            <Button variant="secondary" onClick={handleOnSubmit}>Delete</Button>{' '}
+            {/* <Button variant="secondary" onClick={handleOnSubmit}>Delete</Button>{' '} */}
+
+            <Button variant="secondary" onClick={handleShow}>
+                Delete Review
+            </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                <Modal.Title>Hey! You're about to Delete this review!</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Are you sure you don't want us to know about this place?</Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Never Mind
+                </Button>
+                <Button variant="primary" onClick={handleOnSubmit}>
+                    Yes Please
+                </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     )
 }
